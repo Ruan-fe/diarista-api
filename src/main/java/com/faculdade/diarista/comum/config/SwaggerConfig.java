878b68b9fd8d310b16bfff250c51142dcbfc356d
Ruan-fe <55806119+Ruan-fe@users.noreply.github.com>
2020-12-22 15:12:14 -0300

    Swagger

---
+++ src/main/java/com/faculdade/diarista/comum/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.faculdade.diarista.comum.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@CrossOrigin
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
